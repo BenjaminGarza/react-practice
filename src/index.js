2821fda43e83054464a0fc81d99f210277f60a25
Ben <benjisgarza@gmail.com>
2022-01-31 23:57:04 -0700

    alternate X and O

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
+function Square(props) {
       return (
-        <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
+        <button className="square" onClick={props.onClick}>
+          {props.value}
         </button>
       );
-    }
   }
   
   class Board extends React.Component {
@@ -20,23 +15,28 @@ class Square extends React.Component {
           super(props);
           this.state = {
               squares: Array(9).fill(null),
+              xIsNext: true,
           };
       }
 
       handleClick(i) {
           const squares = this.state.squares.slice();
-          squares[i] = 'X';
-          this.setState({squares: squares});
+          squares[i] = this.state.xIsNext ? 'X' : 'O';
+          this.setState({
+              squares: squares,
+            xIsNext: !this.state.xIsNext,
+            });
       }
-      
+
     renderSquare(i) {
-      return <Square value={this.state.squares[i]}
+      return <Square 
+      value={this.state.squares[i]}
       onClick={() => this.handleClick(i)}
       />;
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
