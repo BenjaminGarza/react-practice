3f9a6022026554847448d40f053d54fc2bbe10c7
Ben <benjisgarza@gmail.com>
2022-02-01 21:57:58 -0700

    calculate winner

--- src/index.js
+++ src/index.js
@@ -36,7 +36,13 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+          status = 'Winner: ' + winner;  
+      } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -77,6 +83,24 @@ function Square(props) {
     }
   }
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i =0; i < lines.length; i++) {
+        const [a,b,c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+          }
+    }
+}
   // ========================================
   
   ReactDOM.render(
